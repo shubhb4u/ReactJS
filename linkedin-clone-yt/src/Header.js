b641a2184d58c6725e5df00b486d1bac1ed4953b
shubhb4u <shubhammazumdar94@gmail.com>
2023-02-23 20:06:07 -0600

    Linkedin-clone

---
+++ linkedin-clone-yt/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchIcon from '@material-ui/icons/Search';
+
+const Header = () => {
+  return (
+    <div>
+        <SearchIcon/>
+    </div>
+  )
+}
+
+export default Header
