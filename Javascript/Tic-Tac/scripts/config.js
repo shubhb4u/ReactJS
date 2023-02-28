885f734f4a145b0566bf15a0fccc66e45db3194d
shubhb4u <shubhammazumdar94@gmail.com>
2023-02-27 20:44:35 -0600

    Tic

--- Javascript/Tic-Tac/scripts/config.js
+++ Javascript/Tic-Tac/scripts/config.js
@@ -1,30 +1,32 @@
 function openPlayerConfig(event) {
-    editedPlayer = +event.target.dataset.playerid; //+'1' -> will be converted to number datatype from string
-    playerConfigOverlayElement.style.display = 'block';
-    backdropElement.style.display = 'block';
-  }
-  
-  function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';
-    formElement.firstElementChild.classList.remove('error');
-    errorsOutputElement.textContent = '';
-  }
-  
-  function savePlayerConfig(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername').trim(); // '      ' => ''
-  
-    if (!enteredPlayername) { // enteredPlayername === ''
-      event.target.firstElementChild.classList.add('error');
-      errorsOutputElement.textContent = 'Please enter a valid name!';
-      return;
-    }
+  editedPlayer = +event.target.dataset.playerid; // +'1' => 1
+  playerConfigOverlayElement.style.display = 'block';
+  backdropElement.style.display = 'block';
+}
 
-    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
-    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = 'none';
+  backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
+  formElement.firstElementChild.lastElementChild.value = '';
+}
 
-    // players[editedPlayer-1].name = enteredPlayername;
-    // closePlayerConfig();
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get('playername').trim(); // '      ' => ''
+
+  if (!enteredPlayername) { // enteredPlayername === ''
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;
   }
+
+  const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();  
+}
