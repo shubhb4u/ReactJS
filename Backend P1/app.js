9afdfb2c2d43e88af578c88d8db97396b4ab07c3
shubhb4u <shubhammazumdar94@gmail.com>
2023-03-28 17:41:53 -0500

    Taking imputs

---
+++ Backend P1/app.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+
+app.get('/', function(req, res) {
+
+    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/about', function(req, res) {
+
+    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+    res.sendFile(htmlFilePath);
+    
+});
+
+app.get('/confirm', function(req, res) {
+
+    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+    res.sendFile(htmlFilePath);
+    
+});
+
+app.get('/restaurants', function(req, res) {
+
+    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/recommend', function(req, res) {
+
+    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.post("/recommend", function(req, res){
+
+    const restaurant = req.body;
+    const filePath = path.join(__dirname, 'data', 'recommend.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    storedRestaurants.push(restaurant);
+
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+    res.redirect('/confirm');
+
+
+})
+
+
+app.listen(3000);
